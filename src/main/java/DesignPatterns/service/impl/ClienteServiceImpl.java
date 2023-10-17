991c842ed687b835c5bf06e0802cc4a4ad089f54
Albérico Eduardo <albericoeduardo202@gmail.com>
2023-10-17 00:54:34 -0300

    crud criado

---
+++ src/main/java/DesignPatterns/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,35 @@
+package DesignPatterns.service.impl;
+
+import DesignPatterns.model.Cliente;
+import DesignPatterns.service.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService{
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void deletar(Long id) {
+        // TODO Auto-generated method stub
+    }
+    
+}
